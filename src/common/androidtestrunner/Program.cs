223d67654ac7b4a66cbb636ab02e8afaa02d78d9
Nathan Ricci <naricc@microsoft.com>
2020-06-09 16:43:59 -0400

    Changed make file.

--- src/common/androidtestrunner/Program.cs
+++ src/common/androidtestrunner/Program.cs
@@ -3,12 +3,13 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Reflection;
 
 public static class Program
 {
     public static int Main(string[] args)
     {
-	var assemblyName = new AssemblyName ("Foo"); // not "Foo.dll", but the name of the assembly
+	var assemblyName = new AssemblyName ("runningmono"); // not "Foo.dll", but the name of the assembly
 	var assembly = Assembly.Load (assemblyName);
 	var mainMethod = assembly.EntryPoint;
 	mainMethod.Invoke (null, args);
